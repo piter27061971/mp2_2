352525bb5f279630175a12c9e0029d94c175336a
piter27061971 <127050097+piter27061971@users.noreply.github.com>
2024-08-30 16:23:18 +0300

    Add files via upload

---
+++ Task_2_2.cpp
@@ -0,0 +1,73 @@
+﻿// Домашнее задание к занятию «Конкуренция, состояние гонки»
+// Задание 2. Прогресс-бар
+
+#include "Timer.h"
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+std::once_flag flag;
+std::mutex m1;
+std::mutex m2;
+std::mutex m3;
+std::mutex m4;
+
+static void tableHeader() { std::cout << "#\t" << "id-thread\t" << "Progress Bar\t\t" << "Time" << std::endl; }
+
+static void progressBar(int numberThreadsNow)
+{
+    int progressBar = 23;
+    int total = 37;
+    int PositionForTime = 43;
+    
+    std::call_once(flag, tableHeader);
+        
+    m2.lock();
+    Timer t1;
+    consol_parameter d1;
+    m2.unlock();
+
+    m4.lock();
+    d1.SetPosition(0, numberThreadsNow + 1);
+    std::cout << numberThreadsNow << "\t" << std::this_thread::get_id() << "\t";
+    m4.unlock();
+
+    while (progressBar < total)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        
+        m1.lock();
+        d1.SetPosition(progressBar, numberThreadsNow + 1);
+        std::cout << char(219);
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        
+        ++progressBar;
+        m1.unlock();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(333));
+    
+    m3.lock();
+    d1.SetPosition(PositionForTime, numberThreadsNow + 1);
+    t1.print();
+    m3.unlock();
+}
+
+int main()
+{
+    int countThreads = 5;
+    std::vector<std::thread> vectorThreads(countThreads);
+    
+    for (size_t i = 0; i < countThreads; i++)
+    {
+        vectorThreads[i] = std::thread(progressBar, i);
+    }
+    for (auto& i : vectorThreads)
+    {
+        i.join();
+    }
+    std::cout << "\n\n\n\nENENE";
+}
